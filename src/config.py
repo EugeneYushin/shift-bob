26414cb0fd8ebf8f44adc3fa04ca4bb90f474bc2
EugenYushin <evgenij.ushin@gmail.com>
2025-02-25 12:11:45 +0300

    introduce HTTP mode

--- src/config.py
+++ src/config.py
@@ -7,6 +7,13 @@ class Impl(StrEnum):
     mem = auto()
 
 
+class SlackMode(StrEnum):
+    http = auto()
+    socket = auto()
+
+
 class Config(BaseModel):
+    mode: SlackMode = SlackMode.socket
+    port: int = 3000
     impl: Impl = Impl.mem
     timezone: str = "America/New_York"
