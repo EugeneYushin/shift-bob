e862487b64dffacf3631ac63339df6aeb4c2d92d
Eugen <evgenij.ushin@gmail.com>
2025-03-05 15:54:41 +0300

    add env vars support for config (#19)

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 from enum import StrEnum, auto
 
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Impl(StrEnum):
@@ -17,9 +18,11 @@ class SQLConfing(BaseModel):
     url: str
 
 
-class Config(BaseModel):
+class Config(BaseSettings):
     mode: SlackMode = SlackMode.socket
     port: int = 3000
     impl: Impl = Impl.sql
     sql: SQLConfing | None = SQLConfing(url="sqlite:///:memory:")
     timezone: str = "America/New_York"
+
+    model_config = SettingsConfigDict(env_prefix="BOB_", env_nested_delimiter="__")
